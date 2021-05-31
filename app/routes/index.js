0292b97022e2c5f9904397edfd01108ceaf77f85
Max Kuchynskyj <youngrake@protonmail.com>
2021-05-31 11:25:52 +0300

    EmberJS tests, filtering, models, adapters, input handling

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,23 +1,10 @@
 import Route from '@ember/routing/route';
-
-const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+  @service store;
+
   async model() {
-    const response = await fetch('/api/rentals.json');
-    let { data } = await response.json();
-
-    return data.map((model) => {
-      let { attributes } = model;
-      let type;
-
-      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-        type = 'Community';
-      } else {
-        type = 'Standalone';
-      }
-
-      return { type, ...attributes };
-    });
+    return this.store.findAll('rental');
   }
 }
